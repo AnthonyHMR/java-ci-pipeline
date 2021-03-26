f7640ed5a83b849b06d18d31c552eaf48654489f
Anthony <anthonymoro08@gmail.com>
2021-03-26 15:41:05 +0000

    Cambio de Prueba
    Prueba Travis

--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        Calculadora calculadora = new Calculadora(10, 4);
+        Calculadora calculadora = new Calculadora(10, 2);
         calculadora.sumar();
         calculadora.restar();
         calculadora.multiplicar();
